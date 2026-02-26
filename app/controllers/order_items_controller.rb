3b5c2a993afa4e71f4017cce90fabe84e955012b
07karthik0704-afk <07karthik.07.04@gmail.com>
2026-02-26 14:33:18 +0530

    completed

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,22 @@
 class OrderItemsController < ApplicationController
 	
 	before_action :authenticate_user!
+	def new 
+		@order=OrderItem.new
+	end
+
+	def create
+        @order=OrderItem.new(orderItem_params)
+        if  @order.save
+            redirect_to "/products" 
+        else
+            render :new
+        end
+    end
+
+    private
+    def orderItem_params
+        params.require(:order_item).permit(:order_id,:product_id,:quantity,:unit_price)
+    end
 
 end
